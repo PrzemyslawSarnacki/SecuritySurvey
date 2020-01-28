3e0b6d90d1f616876bb74d53c73d022731c12aef
PrzemyslawSarnacki <przemos100@gmail.com>
2020-01-28 15:38:26 +0100

    Database update

--- Survey/admin.py
+++ Survey/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from .models import ObjectData, ObjectType, ServiceObject, SecurityDegree, Crimes, DistrictVisually
 
+admin.ModelAdmin.save_as = True
+
 admin.site.register(ObjectData)
 admin.site.register(ObjectType)
 admin.site.register(ServiceObject)
